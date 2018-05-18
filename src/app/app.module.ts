7ea22e4b8961ede573ead32c95db69a9880646e7
Rain-1214 <13131459087@163.com>
2018-05-18 18:05:21 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalendarModule } from './calendar/calendar.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
